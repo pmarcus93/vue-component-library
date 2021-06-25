1bc1a06ec9b91b8588bb92b77bdc05e07f36e30c
Marcus Pereira <marcus.pereira@getrak.com.br>
2021-06-25 14:59:08 -0300

    exporting GdsButton

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+import GdsButton from './components/GdsButton.vue'
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+
+export { GdsButton }
