704338e4ea1ed864c93a3d8ecf9231f14ddbb3cb
Marcus Pereira <marcus.pereira@getrak.com.br>
2021-06-25 14:40:59 -0300

    Adding button.vue.

---
+++ src/components/Button.vue
@@ -0,0 +1,15 @@
+<template>
+    <button>External button component.</button>    
+</template>
+
+<style scoped>
+    button {
+        color: white;
+        font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+        background: palevioletred;
+    }
+</style>
+
+<script>
+    
+</script>
